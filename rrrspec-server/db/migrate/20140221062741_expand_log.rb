4cc332d94b7b92f96351104507b0d9e032a8cfc7
Masaya Suzuki <masaya-suzuki@cookpad.com>
2014-02-25 09:48:17 +0900

    Fix the text length to a valid one

--- rrrspec-server/db/migrate/20140221062741_expand_log.rb
+++ rrrspec-server/db/migrate/20140221062741_expand_log.rb
@@ -1,11 +1,11 @@
 class ExpandLog < ActiveRecord::Migration
   def change
-    change_column(:tasksets, :setup_command, :text, limit: 4294967296)
-    change_column(:tasksets, :slave_command, :text, limit: 4294967296)
-    change_column(:tasksets, :log, :text, limit: 4294967296)
-    change_column(:trials, :stdout, :text, limit: 4294967296)
-    change_column(:trials, :stderr, :text, limit: 4294967296)
-    change_column(:worker_logs, :log, :text, limit: 4294967296)
-    change_column(:slaves, :log, :text, limit: 4294967296)
+    change_column(:tasksets, :setup_command, :text, limit: 4294967295)
+    change_column(:tasksets, :slave_command, :text, limit: 4294967295)
+    change_column(:tasksets, :log, :text, limit: 4294967295)
+    change_column(:trials, :stdout, :text, limit: 4294967295)
+    change_column(:trials, :stderr, :text, limit: 4294967295)
+    change_column(:worker_logs, :log, :text, limit: 4294967295)
+    change_column(:slaves, :log, :text, limit: 4294967295)
   end
 end
