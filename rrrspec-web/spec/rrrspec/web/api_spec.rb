43cf7e8a7896b46088632d6f5b5f7a3f23da686b
Shota Fukumori (sora_h) <sorah@cookpad.com>
2014-04-02 13:23:34 +0900

    Refactor
    - Consolidate same preparing logics
    - use `let!` if available

--- rrrspec-web/spec/rrrspec/web/api_spec.rb
+++ rrrspec-web/spec/rrrspec/web/api_spec.rb
@@ -1,13 +1,7 @@
 require 'spec_helper'
 
 module RRRSpec
-  describe Web::API do
-    include Rack::Test::Methods
-
-    def app
-      Web::API
-    end
-
+  describe "Web" do
     before do
       RRRSpec.configuration = Web::WebConfiguration.new
       RRRSpec.configuration.redis = @redis
@@ -28,310 +22,266 @@ module RRRSpec
       Task.create(taskset, 10, 'spec/test_spec.rb')
     end
 
-    let(:worker) do
+    let!(:worker) do
       Worker.create('default')
     end
 
-    let(:worker_log) do
+    let!(:worker_log) do
       WorkerLog.create(worker, taskset)
     end
 
-    let(:slave) do
+    let!(:slave) do
       Slave.create
     end
 
+    let(:trial) do
+      Trial.create(task, slave)
+    end
+
     before do
-      worker # Create worker
       taskset.add_task(task)
       taskset.enqueue_task(task)
+
       ActiveTaskset.add(taskset)
-      worker_log # Create worker_log
+
       worker_log.set_rsync_finished_time
       worker_log.append_log('worker_log log body')
       worker_log.set_setup_finished_time
-      slave # Create slave
+
       taskset.add_slave(slave)
       slave.append_log('slave log body')
-      trial = Trial.create(task, slave)
+
       trial.start
       trial.finish('pending', 'stdout body', 'stderr body', 10, 2, 0)
+
       task.update_status('pending')
+      taskset.append_log('taskset log body')
+
       taskset.incr_succeeded_count
       taskset.finish_task(task)
       taskset.update_status('succeeded')
       taskset.set_finished_time
+
       ActiveTaskset.remove(taskset)
+
       slave.update_status('normal_exit')
+
       worker_log.set_finished_time
     end
 
-    describe "GET /v1/tasksets/actives" do
-      before { ActiveTaskset.add(taskset) }
+    describe Web::API do
+      include Rack::Test::Methods
 
-      it 'returns the active tasksets' do
-        get "/v1/tasksets/actives"
-        expect(last_response.status).to eq(200)
-        expect(JSON.parse(last_response.body)).to eq(JSON.parse(ActiveTaskset.list.to_json))
+      def app
+        Web::API
       end
-    end
 
-    describe "GET /v1/tasksets/recents" do
-      context 'there are 11 tasksets' do
-        before do
-          11.times { Server::Persistence::Taskset.create() }
-        end
+      describe "GET /v1/tasksets/actives" do
+        before { ActiveTaskset.add(taskset) }
 
-        it 'returns the recent 10 tasksets' do
-          get "/v1/tasksets/recents"
+        it 'returns the active tasksets' do
+          get "/v1/tasksets/actives"
           expect(last_response.status).to eq(200)
-          expect(JSON.parse(last_response.body).size).to eq(10)
+          expect(JSON.parse(last_response.body)).to eq(JSON.parse(ActiveTaskset.list.to_json))
+        end
+      end
+
+      describe "GET /v1/tasksets/recents" do
+        context 'there are 11 tasksets' do
+          before do
+            11.times { Server::Persistence::Taskset.create() }
+          end
+
+          it 'returns the recent 10 tasksets' do
+            get "/v1/tasksets/recents"
+            expect(last_response.status).to eq(200)
+            expect(JSON.parse(last_response.body).size).to eq(10)
+          end
+        end
+      end
+
+      describe "GET /v1/tasksets/:key" do
+        context 'with the taskset persisted' do
+          before do
+            Server::Persister.persist(taskset)
+          end
+
+          it 'returns the taskset' do
+            get "/v1/tasksets/#{taskset.key}"
+            expect(last_response.status).to eq(200)
+            expect(JSON.parse(last_response.body)).to eq(
+              JSON.parse(JSON.generate(Server::Persistence::Taskset.first.as_full_json)).update("is_full" => true)
+            )
+          end
+        end
+
+        context 'with the taskset not persisted' do
+          it 'returns 404' do
+            get "/v1/tasksets/#{taskset.key}"
+            expect(last_response.status).to eq(404)
+          end
+        end
+      end
+
+      describe "GET /v1/batch/tasks/:key" do
+        it 'returns all tasks in the taskset' do
+          get "/v1/batch/tasks/#{taskset.key}"
+          expect(last_response.status).to eq(200)
+          expect(JSON.parse(last_response.body)).to eq([JSON.parse(task.to_json)])
         end
       end
     end
 
-    describe "GET /v1/tasksets/:key" do
-      context 'with the taskset persisted' do
+    describe Web::APIv2 do
+      include Rack::Test::Methods
+
+      def app
+        Web::APIv2
+      end
+
+      describe "GET /v2/tasksets/actives" do
+        before { ActiveTaskset.add(taskset) }
+
+        it 'returns the active tasksets' do
+          get "/v2/tasksets/actives"
+          expect(last_response.status).to eq(200)
+          expect(JSON.parse(last_response.body)).to eq([
+            { 'key' => taskset.key },
+          ])
+        end
+      end
+
+      describe "GET /v2/tasksets/recents" do
+        context 'there are 11 tasksets' do
+          before do
+            11.times { Server::Persistence::Taskset.create() }
+          end
+
+          it 'returns the recent 10 tasksets' do
+            get "/v2/tasksets/recents"
+            expect(last_response.status).to eq(200)
+            expect(JSON.parse(last_response.body).size).to eq(10)
+          end
+        end
+      end
+
+      context 'the taskset is persisted' do
         before do
           Server::Persister.persist(taskset)
         end
 
-        it 'returns the taskset' do
-          get "/v1/tasksets/#{taskset.key}"
-          expect(last_response.status).to eq(200)
-          expect(JSON.parse(last_response.body)).to eq(
-            JSON.parse(JSON.generate(Server::Persistence::Taskset.first.as_full_json)).update("is_full" => true)
-          )
-        end
-      end
-
-      context 'with the taskset not persisted' do
-        it 'returns 404' do
-          get "/v1/tasksets/#{taskset.key}"
-          expect(last_response.status).to eq(404)
-        end
-      end
-    end
-
-    describe "GET /v1/batch/tasks/:key" do
-      it 'returns all tasks in the taskset' do
-        get "/v1/batch/tasks/#{taskset.key}"
-        expect(last_response.status).to eq(200)
-        expect(JSON.parse(last_response.body)).to eq([JSON.parse(task.to_json)])
-      end
-    end
-  end
-
-  describe Web::APIv2 do
-    include Rack::Test::Methods
-
-    def app
-      Web::APIv2
-    end
-
-    before do
-      RRRSpec.configuration = Web::WebConfiguration.new
-      RRRSpec.configuration.redis = @redis
-      RRRSpec.configuration.execute_log_text_path = Dir.mktmpdir
-    end
-
-    after do
-      FileUtils.remove_entry_secure(RRRSpec.configuration.execute_log_text_path)
-    end
-
-    let(:taskset) do
-      Taskset.create(
-        'testuser', 'echo 1', 'echo 2', 'default', 'default', 3, 3, 5, 5
-      )
-    end
-
-    let(:task) do
-      Task.create(taskset, 10, 'spec/test_spec.rb')
-    end
-
-    let(:trial) do
-      task.trials.first
-    end
-
-    let(:worker) do
-      Worker.create('default')
-    end
-
-    let(:worker_log) do
-      WorkerLog.create(worker, taskset)
-    end
-
-    let(:slave) do
-      Slave.create
-    end
-
-    before do
-      worker # Create worker
-      taskset.add_task(task)
-      taskset.enqueue_task(task)
-      ActiveTaskset.add(taskset)
-      worker_log # Create worker_log
-      worker_log.set_rsync_finished_time
-      worker_log.append_log('worker_log log body')
-      worker_log.set_setup_finished_time
-      slave # Create slave
-      taskset.add_slave(slave)
-      slave.append_log('slave log body')
-      trial = Trial.create(task, slave)
-      trial.start
-      trial.finish('pending', 'stdout body', 'stderr body', 10, 2, 0)
-      task.update_status('pending')
-      taskset.append_log('taskset log body')
-      taskset.incr_succeeded_count
-      taskset.finish_task(task)
-      taskset.update_status('succeeded')
-      taskset.set_finished_time
-      ActiveTaskset.remove(taskset)
-      slave.update_status('normal_exit')
-      worker_log.set_finished_time
-    end
-
-    describe "GET /v2/tasksets/actives" do
-      before { ActiveTaskset.add(taskset) }
-
-      it 'returns the active tasksets' do
-        get "/v2/tasksets/actives"
-        expect(last_response.status).to eq(200)
-        expect(JSON.parse(last_response.body)).to eq([
-          { 'key' => taskset.key },
-        ])
-      end
-    end
-
-    describe "GET /v2/tasksets/recents" do
-      context 'there are 11 tasksets' do
-        before do
-          11.times { Server::Persistence::Taskset.create() }
+        describe 'GET /v2/tasksets/:taskset_key' do
+          it 'returns a taskset in JSON' do
+            get "/v2/tasksets/#{taskset.key}"
+            expect(last_response.status).to eq(200)
+            expect(JSON.parse(last_response.body)).to eq({
+              "created_at" => taskset.created_at.iso8601,
+              "finished_at" => taskset.finished_at.iso8601,
+              "id" => 1,
+              "key" => taskset.key,
+              "max_trials" => 3,
+              "max_workers" => 3,
+              "rsync_name" => "testuser",
+              "setup_command" => "echo 1",
+              "slave_command" => "echo 2",
+              "status" => "succeeded",
+              "tasks" => [
+                {
+                  "id" => 1,
+                  "key" => task.key,
+                  "status" => "pending",
+                  "spec_path" => "spec/test_spec.rb",
+                  "estimate_sec" => 10,
+                  "trials" => [
+                    {
+                      "id" => 1,
+                      "key" => trial.key,
+                      "task_id" => 1,
+                      "slave_id" => 1,
+                      "started_at" => trial.started_at.iso8601,
+                      "finished_at" => trial.finished_at.iso8601,
+                      "status" => "pending",
+                      "passed" => 10,
+                      "pending" => 2,
+                      "failed" => 0
+                    },
+                  ],
+                },
+              ],
+              "taskset_class" => "default",
+              "worker_type" => "default",
+            })
+          end
         end
 
-        it 'returns the recent 10 tasksets' do
-          get "/v2/tasksets/recents"
-          expect(last_response.status).to eq(200)
-          expect(JSON.parse(last_response.body).size).to eq(10)
+        describe 'GET /v2/tasksets/:taskset_id/log' do
+          it 'returns a string in JSON' do
+            get "/v2/tasksets/1/log"
+            expect(last_response.status).to eq(200)
+            expect(JSON.parse(last_response.body)).to eq({
+              'log' => 'taskset log body',
+            })
+          end
         end
-      end
-    end
 
-    context 'the taskset is persisted' do
-      before do
-        Server::Persister.persist(taskset)
-      end
-
-      describe 'GET /v2/tasksets/:taskset_key' do
-        it 'returns a taskset in JSON' do
-          get "/v2/tasksets/#{taskset.key}"
-          expect(last_response.status).to eq(200)
-          expect(JSON.parse(last_response.body)).to eq({
-            "created_at" => taskset.created_at.iso8601,
-            "finished_at" => taskset.finished_at.iso8601,
-            "id" => 1,
-            "key" => taskset.key,
-            "max_trials" => 3,
-            "max_workers" => 3,
-            "rsync_name" => "testuser",
-            "setup_command" => "echo 1",
-            "slave_command" => "echo 2",
-            "status" => "succeeded",
-            "tasks" => [
+        describe 'GET /v2/tasks/:task_id/trials' do
+          it 'returns trials in JSON' do
+            get "/v2/tasks/1/trials"
+            expect(last_response.status).to eq(200)
+            expect(JSON.parse(last_response.body)).to eq([
               {
                 "id" => 1,
-                "key" => task.key,
+                "key" => trial.key,
+                "task_id" => 1,
+                "slave_id" => 1,
+                "started_at" => trial.started_at.iso8601,
+                "finished_at" => trial.finished_at.iso8601,
                 "status" => "pending",
-                "spec_path" => "spec/test_spec.rb",
-                "estimate_sec" => 10,
+                "passed" => 10,
+                "pending" => 2,
+                "failed" => 0,
+              },
+            ])
+          end
+        end
+
+        describe 'GET /v2/tasksets/:taskset_id/worker_logs' do
+          it 'returns worker logs in JSON' do
+            get "/v2/tasksets/1/worker_logs"
+            expect(last_response.status).to eq(200)
+            expect(JSON.parse(last_response.body)).to eq([
+              {
+                "id" => 1,
+                "worker_name" => "rrrspec:worker:testhostname",
+                "started_at" => worker_log.started_at.iso8601,
+                "rsync_finished_at" => worker_log.rsync_finished_at.iso8601,
+                "setup_finished_at" => worker_log.setup_finished_at.iso8601,
+                "rspec_finished_at" => worker_log.finished_at.iso8601,
+                "log" => "worker_log log body",
+              },
+            ])
+          end
+        end
+
+        describe 'GET /v2/tasksets/:taskset_id/slaves' do
+          it 'returns slaves in JSON' do
+            get "/v2/tasksets/1/slaves"
+            expect(last_response.status).to eq(200)
+            expect(JSON.parse(last_response.body)).to eq([
+              {
+                "id" => 1,
+                "name" => slave.key,
+                "status" => "normal_exit",
                 "trials" => [
                   {
                     "id" => 1,
                     "key" => trial.key,
-                    "task_id" => 1,
-                    "slave_id" => 1,
-                    "started_at" => trial.started_at.iso8601,
-                    "finished_at" => trial.finished_at.iso8601,
-                    "status" => "pending",
-                    "passed" => 10,
-                    "pending" => 2,
-                    "failed" => 0
                   },
                 ],
+                "log" => "slave log body",
               },
-            ],
-            "taskset_class" => "default",
-            "worker_type" => "default",
-          })
-        end
-      end
-
-      describe 'GET /v2/tasksets/:taskset_id/log' do
-        it 'returns a string in JSON' do
-          get "/v2/tasksets/1/log"
-          expect(last_response.status).to eq(200)
-          expect(JSON.parse(last_response.body)).to eq({
-            'log' => 'taskset log body',
-          })
-        end
-      end
-
-      describe 'GET /v2/tasks/:task_id/trials' do
-        it 'returns trials in JSON' do
-          get "/v2/tasks/1/trials"
-          expect(last_response.status).to eq(200)
-          expect(JSON.parse(last_response.body)).to eq([
-            {
-              "id" => 1,
-              "key" => trial.key,
-              "task_id" => 1,
-              "slave_id" => 1,
-              "started_at" => trial.started_at.iso8601,
-              "finished_at" => trial.finished_at.iso8601,
-              "status" => "pending",
-              "passed" => 10,
-              "pending" => 2,
-              "failed" => 0,
-            },
-          ])
-        end
-      end
-
-      describe 'GET /v2/tasksets/:taskset_id/worker_logs' do
-        it 'returns worker logs in JSON' do
-          get "/v2/tasksets/1/worker_logs"
-          expect(last_response.status).to eq(200)
-          expect(JSON.parse(last_response.body)).to eq([
-            {
-              "id" => 1,
-              "worker_name" => "rrrspec:worker:testhostname",
-              "started_at" => worker_log.started_at.iso8601,
-              "rsync_finished_at" => worker_log.rsync_finished_at.iso8601,
-              "setup_finished_at" => worker_log.setup_finished_at.iso8601,
-              "rspec_finished_at" => worker_log.finished_at.iso8601,
-              "log" => "worker_log log body",
-            },
-          ])
-        end
-      end
-
-      describe 'GET /v2/tasksets/:taskset_id/slaves' do
-        it 'returns slaves in JSON' do
-          get "/v2/tasksets/1/slaves"
-          expect(last_response.status).to eq(200)
-          expect(JSON.parse(last_response.body)).to eq([
-            {
-              "id" => 1,
-              "name" => slave.key,
-              "status" => "normal_exit",
-              "trials" => [
-                {
-                  "id" => 1,
-                  "key" => trial.key,
-                },
-              ],
-              "log" => "slave log body",
-            },
-          ])
+            ])
+          end
         end
       end
     end
